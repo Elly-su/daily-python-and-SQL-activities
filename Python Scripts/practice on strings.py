ee4b21367ab256369152a673068fdcf1ef8b8fae
Elly Ochieng <ellyaston404@gmail.com>
2025-02-05 10:31:01 +0300

    `Added new string formatting examples and functions to practice on strings.py`

--- Python Scripts/practice on strings.py
+++ Python Scripts/practice on strings.py
@@ -38,3 +38,26 @@ print(txt)
 txt =f"The price is {price:.2f}"
 txt= f"It is very {'Expensive'if price > 50 else 'Cheap'}"
 print(txt)
+fruit = "banana"
+txt= f"I love {fruit.upper()}!"
+print(txt)
+
+def myconverter(x):
+    return x * 0.3048
+txt =f"the plane is flying at a {myconverter(30000)} meter altitude"
+print(txt)
+
+price = 59000
+txt = f"The price is {price:,}"
+print(txt)
+
+price = 49
+txt = "The price is {} dollars "
+print(txt.format(price))
+
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
