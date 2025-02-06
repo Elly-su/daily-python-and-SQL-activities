2a233d865e69e9c6901e98b28e9c4d98dde039d2
Elly Ochieng <ellyaston404@gmail.com>
2025-02-06 14:11:00 +0300

    Refactor list variable names and add new list manipulation examples;

--- Python Scripts/lists.py
+++ Python Scripts/lists.py
@@ -1,6 +1,6 @@
-thisList = ["apple", "banana", "cherry"]
-print(thisList)
-print(len(thisList))
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+print(len(thislist))
 
 list1 = ["apple", "banana", "cherry"]
 list2 = [1, 5, 7, 9, 3]
@@ -25,6 +25,61 @@ print(thislist)
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
 thislist[1:3] = ["blackcurrant", "watermelon"]
 print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+thislist.append("grapes")
+print(thislist)
+
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)  
+print(thislist)
+
+thislist.remove("banana")
+print(thislist)
+
+thislist1 = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist1)
+
+del thislist1[0]
+print(thislist1)
+
+thislist1.clear()
+print(thislist1)
+
+for x in thislist:
+  print(x)
+  
+  for i in range (len(thislist)):
+    print(thislist[i])
+    i = 0
+    while i < len(thislist):
+      print(thislist[i])
+      i = i + 1
+      
+      [print(x) for x in thislist]
+      fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+      newlist = []
+      for x in fruits:
+        if "a" in x:
+          newlist.append(x)
+        
+        print(newlist)
+        
+        newlist = [x for x in fruits if "a" in x]
+        print(newlist)
+      
 
 
 
