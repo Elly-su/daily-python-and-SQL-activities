693a4b4d7706ca4d2689227f66f777ec3a676cad
Elly Ochieng <ellyaston404@gmail.com>
2025-02-07 11:50:47 +0300

    Add list sorting and joining examples with method descriptions

--- Python Scripts/lists.py
+++ Python Scripts/lists.py
@@ -79,7 +79,48 @@ for x in thislist:
         
         newlist = [x for x in fruits if "a" in x]
         print(newlist)
-      
+        
+        #sorting lists
+        thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+        thislist.sort()
+        print(thislist)
+        
+        thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+  
+def myfunc(n):
+    return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+#joining lists
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+#Method	Description
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
 
 
 
