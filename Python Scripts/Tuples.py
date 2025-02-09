c36e1aa3a4747422226f256cdfc933c89c6a5539
Elly Ochieng <ellyaston404@gmail.com>
2025-02-09 11:59:37 +0300

    Add examples and explanations for tuples in Python

---
+++ Python Scripts/Tuples.py
@@ -0,0 +1,50 @@
+#Tuples in python
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+# When we say that tuples are ordered, it means that the items have a defined order, and that order will not change.
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+print(len(thistuple))
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+ 
+
+x = ("apple", "banana", "cherry")#convert the tuple into a list to be able to change it
+y = list(x) #convert the tuple into a list to be able to change it
+y[1] = "kiwi"#change the second item
+x = tuple(y)#convert the list back into a tuple
+
+print(x)
+
+thistuple = ("apple", "banana", "cherry")#add items
+y = list(thistuple)#convert the tuple into a list to be able to change it
+y.append("orange")#add an item
+thistuple = tuple(y)#convert the list back into a tuple
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")#remove an item from a tuple by converting it into a list
+thistuple = tuple(y)
