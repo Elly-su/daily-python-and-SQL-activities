9420f880928f30a377ebc57d27ec390c7b0521ee
Elly Ochieng <ellyaston404@gmail.com>
2025-02-10 10:39:12 +0300

    Add examples and explanations for sets in Python

---
+++ Python Scripts/sets.py
@@ -0,0 +1,7 @@
+myset = {"apple", "banana", "cherry"}
+print(myset)
+#sets are used to store mutiple items in a single variable
+thissset = {"apple", "banana", "cherry"}
+print(thissset)
+#sets are unordered, so the items will appear in a random order
+print(len(thissset))
