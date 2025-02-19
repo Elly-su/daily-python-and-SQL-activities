28d17fdcd399e2c6cb00d1dd32da3e2c5485aa3a
Elly Ochieng <ellyaston404@gmail.com>
2025-02-19 15:22:53 +0300

    Added a while loop and a for loop to loops.py

--- Python Scripts/loops.py
+++ Python Scripts/loops.py
@@ -9,12 +9,11 @@ while i < 6:
   if i == 3:
     break
   i += 1
-  
-i = 1
-while i < 6:
-  print(i)
-  i += 1
-else:
-  print("i is no longer less than 6")
-  
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+
   
